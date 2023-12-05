9d0f75724608b2932b7aae4383db98f675e7ffc2
LEANDRO MORAES DE ALECRIM <lalecrim@tjsp.jus.br>
2023-12-05 14:45:23 -0300

    clear app template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'customized-components';
+  title = 'Customized Components';
 }
